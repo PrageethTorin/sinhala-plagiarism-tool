8a597186bec1147d4a8456db4a04507805529476
Prageeth Torin <148193416+PrageethTorin@users.noreply.github.com>
2025-11-22 09:31:18 +0530

    change icon

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import myIcon from './assets/myicon.png';
+
+// Set the favicon at runtime from the bundled asset (uses webpack/file-loader)
+function setFavicon(url) {
+  try {
+    let link = document.querySelector("link[rel*='icon']");
+    if (!link) {
+      link = document.createElement('link');
+      link.rel = 'icon';
+      document.getElementsByTagName('head')[0].appendChild(link);
+    }
+    link.type = 'image/png';
+    link.href = url;
+  } catch (e) {
+    
+  }
+}
+
+setFavicon(myIcon);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
