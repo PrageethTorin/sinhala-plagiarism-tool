54bdf7c02bfce376ec974cdbd7a7a475913234fa
Prageeth Torin <148193416+PrageethTorin@users.noreply.github.com>
2025-12-07 09:18:41 +0530

    fix error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Home from './components/Home';
 import Login from './components/Login';
 import Paraphrase from './components/Paraphrase';
-import SemanticSimilarity from './components/SemanticSimilarity';
+//import SemanticSimilarity from './components/SemanticSimilarity';
 import Pretrained from './components/Pretrained';
 
 function Feature({ title }) {
@@ -31,7 +31,7 @@ function App() {
   if (path === '/login') return <Login />;
   if (path === '/paraphrase') return <Paraphrase />;
   if (path === '/writing-style-1') return <Feature title="Writing Style" />;
-  if (path === '/writing-style-2') return <SemanticSimilarity sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />;
+  //if (path === '/writing-style-2') return <SemanticSimilarity sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />;
   if (path === '/writing-style-3') return <Feature title="Writing Style" />;
   
 
