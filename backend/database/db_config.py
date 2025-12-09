7f45b83c2c8cbb9e9719899cfded7e9f9c293905
Asela Wanigabadu <kavindaasela18@gmail.com>
2025-12-10 01:20:18 +0530

    backend setup

--- backend/database/db_config.py
+++ backend/database/db_config.py
@@ -5,7 +5,7 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",        # Change to your MySQL username
-        password="root",# Change to your MySQL password
+        password="Asela@123",# Change to your MySQL password
         database="sinhala_plagiarism_db",
         charset='utf8mb4'   # Critical for Sinhala font support
     )
